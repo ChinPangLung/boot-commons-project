217ad43716af4123e8b7d6928e738f7ba4f8cb59
Lung <chinpang9527@gmail.com>
2019-05-27 16:13:22 +0800

    添加相关的注解说明

--- boot-commons-utils/src/main/java/com/lung/application/utils/redis/RedisUtil.java
+++ boot-commons-utils/src/main/java/com/lung/application/utils/redis/RedisUtil.java
@@ -365,8 +365,9 @@ public class RedisUtil {
     public long sSetAndTime(String key, long time, Object... values) {
         try {
             Long count = redisTemplate.opsForSet().add(key, values);
-            if (time > 0)
+            if (time > 0) {
                 expire(key, time);
+            }
             return count;
         } catch (Exception e) {
             e.printStackTrace();
@@ -484,8 +485,9 @@ public class RedisUtil {
     public boolean lSet(String key, Object value, long time) {
         try {
             redisTemplate.opsForList().rightPush(key, value);
-            if (time > 0)
+            if (time > 0) {
                 expire(key, time);
+            }
             return true;
         } catch (Exception e) {
             e.printStackTrace();
@@ -521,8 +523,9 @@ public class RedisUtil {
     public boolean lSet(String key, List<Object> value, long time) {
         try {
             redisTemplate.opsForList().rightPushAll(key, value);
-            if (time > 0)
+            if (time > 0) {
                 expire(key, time);
+            }
             return true;
         } catch (Exception e) {
             e.printStackTrace();
