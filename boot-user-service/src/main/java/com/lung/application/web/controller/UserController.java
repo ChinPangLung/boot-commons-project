6da449c4a389d4cc84ae04715f1fd9b6556e9997
Lung <chinpang9527@gmail.com>
2019-05-30 15:40:01 +0800

    实现user-service和order-service两个服务相互调用  实现微服务

--- boot-user-service/src/main/java/com/lung/application/web/controller/UserController.java
+++ boot-user-service/src/main/java/com/lung/application/web/controller/UserController.java
@@ -1,16 +1,22 @@
 package com.lung.application.web.controller;
 
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.lung.application.model.entity.Account;
 import com.lung.application.model.entity.User;
+import com.lung.application.service.IAccountService;
 import com.lung.application.service.IUserService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -22,15 +28,24 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/user")
+@Log4j2
 public class UserController {
 
     @Autowired
     private IUserService userService;
 
+    @Reference
+    private IAccountService accountService;
+
     @RequestMapping(value = "list")
-    public List<User> findAllUser() {
+    public Map<String, Object> findAllUser() {
+        Map<String, Object> result = new HashMap<>();
         List<User> users = userService.selectList(new EntityWrapper<>());
-        return users;
+        log.info("调用order-service的account接口=======");
+        List<Account> accounts = accountService.selectList(new EntityWrapper<>());
+        result.put("user", users);
+        result.put("account", accounts);
+        return result;
     }
 
 }
