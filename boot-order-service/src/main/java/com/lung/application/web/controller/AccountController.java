3ae715c3bdc0edcc6f2f507a921ff6e67171e102
Lung <chinpang9527@gmail.com>
2019-07-18 21:27:58 +0800

    update

--- boot-order-service/src/main/java/com/lung/application/web/controller/AccountController.java
+++ boot-order-service/src/main/java/com/lung/application/web/controller/AccountController.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author ChinPangLung
@@ -36,8 +36,16 @@ public class AccountController {
     @Reference
     private IUserService userService;
 
+    /**
+     * findAccountList
+     *
+     * @param name
+     * @return java.util.Map<java.lang.String, java.lang.Object>
+     * @author longzhanpeng chinpang9527@gmail.com
+     * @since 19-7-18
+     */
     @RequestMapping(value = "findaccount")
-    public Map<String,Object> findAccountList(String name) {
+    public Map<String, Object> findAccountList(String name) {
 
         Map<String, Object> result = new HashMap<>(0b10000);
         log.info("首先测试account接口本地调用==========");
